d2a0c7248e6615ca5cf4c626fd5241200ede1a06
Li <jiarong.li@kadaster.nl>
2021-02-26 14:36:33 +0100

    change homepage to .

---
+++ demonstrators/namen-app/precache-manifest.5408952e14f3b148be8adab9a06d8eec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d470b922ec080667450039582e4566",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fabc1b6fda563e15dbab",
+    "url": "./static/css/2.c1210ddd.chunk.css"
+  },
+  {
+    "revision": "66ab6ae9ce5d846d7dc1",
+    "url": "./static/css/main.dba41ac2.chunk.css"
+  },
+  {
+    "revision": "fabc1b6fda563e15dbab",
+    "url": "./static/js/2.379a55c0.chunk.js"
+  },
+  {
+    "revision": "66ab6ae9ce5d846d7dc1",
+    "url": "./static/js/main.f594c7c8.chunk.js"
+  },
+  {
+    "revision": "eb5bad2c368cc7f8becf",
+    "url": "./static/js/runtime-main.8402b9e8.js"
+  },
+  {
+    "revision": "5d975e1bbbefb9eb55fbad23ead48aab",
+    "url": "./static/media/Logo-kadaster.5d975e1b.png"
+  }
+]);
