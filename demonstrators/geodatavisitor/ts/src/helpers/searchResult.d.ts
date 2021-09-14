2b087c998a1831837cd23b03106673a4d7ea3a55
Li <j.li@kadaster.nl>
2021-09-14 17:18:53 +0200

    update GeoDataVisitor

--- demonstrators/geodatavisitor/ts/src/helpers/searchResult.d.ts
+++ demonstrators/geodatavisitor/ts/src/helpers/searchResult.d.ts
@@ -1,2 +1,1 @@
-export declare function getFromCoordinates(lat: string, lng: string): Promise<import("../reducer").SingleObject[]>;
 export declare function getFromTextSearch(api: string): Promise<import("../reducer").SingleObject[]>;
