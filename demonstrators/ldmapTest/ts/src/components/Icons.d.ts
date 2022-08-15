b1034c5ca93252b892540cde845ef580a3e69c87
kad-haanam <63039628+kad-haanam@users.noreply.github.com>
2022-08-15 17:40:15 +0200

    delete old repo

--- demonstrators/ldmapTest/ts/src/components/Icons.d.ts
+++
@@ -1,3 +0,0 @@
-import L from "leaflet";
-export declare const Icons: L.Icon.Default;
-export declare const DefaultIcon: L.Icon.Default;
--- demonstrators/ldmapTest/ts/src/components/StartMessage.d.ts
+++
@@ -1,3 +0,0 @@
-import React from "react";
-declare const StartMessage: React.FC;
-export default StartMessage;
